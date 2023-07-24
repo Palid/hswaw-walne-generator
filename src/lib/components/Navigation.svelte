bbcdc7d6dc3a0ddc46dd91ebd8db477080a99925
Dariusz Niemczyk <palid@phoelid.me>
2023-07-24 02:11:06 +0200

    Add better navigation and mermaid

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+
+	import Fa from 'svelte-fa/src/fa.svelte';
+	import { faUsers, faFileSignature, faCheckToSlot } from '@fortawesome/free-solid-svg-icons';
+	import Syrenka from '$lib/components/HSMermaid.svelte';
+	import { page } from '$app/stores';
+
+	export let currentTile: number = 0;
+</script>
+
+<AppRail>
+	<!-- --- -->
+	<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+		<svelte:fragment slot="lead">
+			<Fa class="w-full" icon={faUsers} />
+		</svelte:fragment>
+		<p>In-person checklist</p>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+		<svelte:fragment slot="lead">
+			<Fa class="w-full" icon={faFileSignature} />
+		</svelte:fragment>
+		<p>Passed-permissions voters</p>
+	</AppRailTile>
+	<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+		<svelte:fragment slot="lead">
+			<Fa class="w-full" icon={faCheckToSlot} />
+		</svelte:fragment>
+		<p>Create poll</p>
+	</AppRailTile>
+	<!-- --- -->
+	<svelte:fragment slot="trail">
+		<AppRailAnchor href="https://hackerspace.pl" target="_blank" title="Account">
+			<svelte:fragment slot="lead"><Syrenka /></svelte:fragment>
+		</AppRailAnchor>
+	</svelte:fragment>
+</AppRail>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
@@ -6,44 +6,53 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open({});
+	}
 </script>
 
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">Meetings poll generator</strong>
+				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="https://hackerspace.pl/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					Hackerspace.pl
 				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
-	<slot />
+	<svelte:fragment slot="sidebarLeft">
+		<div id="sidebar-left" class="hidden lg:block h-full">
+			<Navigation />
+		</div>
+	</svelte:fragment>
+	<div class="container h-full mx-auto flex justify-center items-center">
+		<Drawer><Navigation /></Drawer>
+		<slot />
+	</div>
 </AppShell>
